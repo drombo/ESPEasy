a00e449cfa306df6cc94ee0c988923d5987de76f
Bartlomiej Zimon <uzi18@o2.pl>
2018-02-24 10:19:12 +0100

    - extend conditions with AND/OR, now it is possible to simplify rules
    test rules:
    on test do
      if [test#a]=0 or [test#b]=0 or [test#c]=0
        event ok
      else
        event nok
      endif
    endon
    
    on test2 do
      if [test#a]=1 and [test#b]=1 and [test#c]=1
        event ok
      else
        event nok
      endif
    endon
    
    on test3 do
      if [test#a]=1 and [test#b]=1 or [test#c]=0
        event ok
      else
        event nok
      endif
    endon
    
    on test4 do
      if [test#a]=0
        event ok
      else
        event nok
      endif
    endon

--- src/Misc.ino
+++ src/Misc.ino
@@ -2303,6 +2303,7 @@ boolean conditionMatchExtended(String& check) {
 		if (condAnd > 0 || condOr > 0) { // we got AND/OR
 			if (condAnd > 0	&& ((condOr < 0 && condOr < condAnd) || (condOr > 0 && condOr > condAnd))) { //AND is first
 				check = check.substring(condAnd + 5);
+				Serial.println(check);
 				rightcond = conditionMatch(check);
 				leftcond = (leftcond && rightcond);
 			} else { //OR is first
@@ -2315,7 +2316,7 @@ boolean conditionMatchExtended(String& check) {
 	return leftcond;
 }
 
-boolean conditionMatch(const String& check)
+boolean conditionMatch(String& check)
 {
   boolean match = false;
 
