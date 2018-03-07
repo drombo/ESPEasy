77dcad87f16280268812aeb93f0aeaba206209bb
drombo <drombo@drombo.de>
2018-03-07 08:34:09 +0100

    some rework

--- src/_P242_CNY70.ino
+++ src/_P242_CNY70.ino
@@ -13,6 +13,10 @@
 
 uint8_t Plugin_242_MeasurementCount = 0; // counter for measurements to calculate average
 uint32_t Plugin_242_valueSum = 0;        // sum of values to calculate average
+float Plugin_242_avgValue = 0;
+
+uint32_t Plugin_242_avgValuePrevious = 0;        // sum of values to calculate average
+int16_t Plugin_242_cumulativeSum = 0;
 
 unsigned long Plugin_242_triggerTime;
 unsigned long Plugin_242_triggerTimePrevious;
@@ -27,135 +31,161 @@ boolean Plugin_242(byte function, struct EventStruct *event, String &string)
 
   switch (function)
   {
-  case PLUGIN_DEVICE_ADD:
-  {
-    Device[++deviceCount].Number = PLUGIN_ID_242;
-    Device[deviceCount].Type = DEVICE_TYPE_SINGLE;
-    Device[deviceCount].VType = SENSOR_TYPE_TRIPLE;
-    Device[deviceCount].Ports = 0;
-    Device[deviceCount].PullUpOption = false;
-    Device[deviceCount].InverseLogicOption = false;
-    Device[deviceCount].FormulaOption = true;
-    Device[deviceCount].ValueCount = 3;
-    Device[deviceCount].SendDataOption = true;
-    Device[deviceCount].TimerOption = false;
-    Device[deviceCount].GlobalSyncOption = true;
-    break;
-  }
-
-  case PLUGIN_GET_DEVICENAME:
-  {
-    string = F(PLUGIN_NAME_242);
-    break;
-  }
-
-  case PLUGIN_GET_DEVICEVALUENAMES:
-  {
-    strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_242));
-    strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_242));
-    strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME3_242));
-    break;
-  }
-
-  case PLUGIN_INIT:
-  {
-
-    if (! Settings.TaskDevicePluginConfig[event->TaskIndex][0])
+    case PLUGIN_DEVICE_ADD:
     {
-      UserVar[event->BaseVarIndex + 2] = 0;
+      Device[++deviceCount].Number = PLUGIN_ID_242;
+      Device[deviceCount].Type = DEVICE_TYPE_SINGLE;
+      Device[deviceCount].VType = SENSOR_TYPE_TRIPLE;
+      Device[deviceCount].Ports = 0;
+      Device[deviceCount].PullUpOption = false;
+      Device[deviceCount].InverseLogicOption = false;
+      Device[deviceCount].FormulaOption = true;
+      Device[deviceCount].ValueCount = 3;
+      Device[deviceCount].SendDataOption = true;
+      Device[deviceCount].TimerOption = false;
+      Device[deviceCount].GlobalSyncOption = true;
+      break;
     }
 
-    String log = F("connect IR Led to PIN: ");
-    log += Settings.TaskDevicePin1[event->TaskIndex];
-    addLog(LOG_LEVEL_INFO, log);
-    success = true;
-    break;
-  }
+    case PLUGIN_GET_DEVICENAME:
+    {
+      string = F(PLUGIN_NAME_242);
+      break;
+    }
 
-  case PLUGIN_WEBFORM_LOAD:
-  {
+    case PLUGIN_GET_DEVICEVALUENAMES:
+    {
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_242));
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_242));
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME3_242));
+      break;
+    }
 
-    addFormCheckBox(string, F("Send raw data"), F("plugin_242_sendraw"), Settings.TaskDevicePluginConfig[event->TaskIndex][0]);
-    addFormNote(string, F("on for calibration"));
+    case PLUGIN_INIT:
+    {
 
-    addFormTextBox(string, F("Trigger Level"), F("plugin_242_triggerlevel"), String(Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0]), 8);
+      if (! Settings.TaskDevicePluginConfig[event->TaskIndex][0])
+      {
+        UserVar[event->BaseVarIndex + 2] = 0;
+      }
 
-    addFormTextBox(string, F("Hysteresis"), F("plugin_242_hyst"), String(Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1]), 8);
+      String log = F("connect IR Led to PIN: ");
+      log += Settings.TaskDevicePin1[event->TaskIndex];
+      addLog(LOG_LEVEL_INFO, log);
+      success = true;
+      break;
+    }
 
-    success = true;
-    break;
-  }
+    case PLUGIN_WEBFORM_LOAD:
+    {
 
-  case PLUGIN_WEBFORM_SAVE:
-  {
-    Settings.TaskDevicePluginConfig[event->TaskIndex][0] = isFormItemChecked(F("plugin_242_sendraw"));
-    Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0] = getFormItemFloat(F("plugin_242_triggerlevel"));
-    Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1] = getFormItemFloat(F("plugin_242_hyst"));
+      addFormCheckBox(string, F("Send raw data"), F("plugin_242_sendraw"), Settings.TaskDevicePluginConfig[event->TaskIndex][0]);
+      addFormNote(string, F("on for calibration"));
 
-    success = true;
-    break;
-  }
+      addFormTextBox(string, F("Trigger Level"), F("plugin_242_triggerlevel"), String(Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0]), 8);
 
-  case PLUGIN_TEN_PER_SECOND:
-  {
-    String debuglog = F("");
-    String log = F("");
+      addFormTextBox(string, F("Hysteresis"), F("plugin_242_hyst"), String(Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1]), 8);
 
-    // perform measurement with IR LED off
-    digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], LOW);
-    // wait 10 milliseconds
-    delay(10);
-    // read the analog in value:
-    int16_t Plugin_242_sensorValueOff = analogRead(A0);
+      success = true;
+      break;
+    }
 
-    // turn IR LED back on
-    digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], HIGH);
-    delay(10);
-    // read the analog in value:
-    int16_t Plugin_242_sensorValueOn = analogRead(A0);
+    case PLUGIN_WEBFORM_SAVE:
+    {
+      Settings.TaskDevicePluginConfig[event->TaskIndex][0] = isFormItemChecked(F("plugin_242_sendraw"));
+      Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0] = getFormItemFloat(F("plugin_242_triggerlevel"));
+      Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1] = getFormItemFloat(F("plugin_242_hyst"));
 
-    // calculate difference
-    int16_t Plugin_242_sensorValue = Plugin_242_sensorValueOff - Plugin_242_sensorValueOn;
+      success = true;
+      break;
+    }
 
-    // build sums for average calculation
-    Plugin_242_MeasurementCount++;
-    Plugin_242_valueSum += Plugin_242_sensorValue;
+    case PLUGIN_TEN_PER_SECOND:
+    {
+      String debuglog = F("");
 
-    // debuglog += F("Raw: ");
-    // debuglog += Plugin_242_sensorValue;
+      // perform measurement with IR LED off
+      digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], LOW);
+      // wait 10 milliseconds
+      delay(10);
+      // read the analog in value:
+      int16_t Plugin_242_sensorValueOff = analogRead(A0);
 
-    if (Plugin_242_MeasurementCount >= Plugin_242_avgCounterMax)
-    { 
+      // turn IR LED back on
+      digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], HIGH);
+      delay(10);
+      // read the analog in value:
+      int16_t Plugin_242_sensorValueOn = analogRead(A0);
 
-      float avgValue = (float)(Plugin_242_valueSum / Plugin_242_MeasurementCount);
-      UserVar[event->BaseVarIndex + 2] = avgValue;
+      // calculate difference
+      int16_t Plugin_242_sensorValue = Plugin_242_sensorValueOff - Plugin_242_sensorValueOn;
+
+      // build sums for average calculation
+      Plugin_242_MeasurementCount++;
+      Plugin_242_valueSum += Plugin_242_sensorValue;
+
+      Plugin_242_avgValue = (float)(Plugin_242_valueSum / Plugin_242_MeasurementCount);
+
+      debuglog += F("Raw Value: ");
+      debuglog += Plugin_242_sensorValue;
+      debuglog += F(" avgValue: ");
+      debuglog += Plugin_242_avgValue;
+
+      addLog(LOG_LEVEL_DEBUG, debuglog);
+
+      success = true;
+      break;
+    }
+
+    case PLUGIN_ONCE_A_SECOND:
+    {
+      String log = F("");
+
+      UserVar[event->BaseVarIndex + 2] = Plugin_242_avgValue;
+
+      int16_t difference = 0;
+
+      // Previous value is 0 after init. Do not use this value for calculation.
+      if(Plugin_242_avgValuePrevious != 0){
+        difference = Plugin_242_avgValue - Plugin_242_avgValuePrevious;
+      }
+
+      Plugin_242_cumulativeSum += difference;
+
+      log += F("Avg: ");
+      log += Plugin_242_avgValue;
+      log += F(" Difference: ");
+      log += difference;
+      log += F(" Cumulative Sum: ");
+      log += Plugin_242_cumulativeSum;
+
 
       boolean state = Plugin_242_triggerstate;
 
       // compare with threshold value
       float valueLowThreshold = Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0] - (Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1] / 2);
       float valueHighThreshold = Settings.TaskDevicePluginConfigFloat[event->TaskIndex][0] + (Settings.TaskDevicePluginConfigFloat[event->TaskIndex][1] / 2);
-      if (avgValue <= valueLowThreshold)
-        state = false;
-      if (avgValue >= valueHighThreshold)
-        state = true;
+      if (Plugin_242_cumulativeSum <= valueLowThreshold)
+      state = false;
+      if (Plugin_242_cumulativeSum >= valueHighThreshold)
+      state = true;
 
       // if value changed
       if (state != Plugin_242_triggerstate)
       {
-        log += F(" Trigger changed state: ");
         Plugin_242_triggerstate = state;
         //statusLED(true);
         UserVar[event->BaseVarIndex] = state ? 1 : 0;
 
+        log += F(" Trigger changed state: ");
         log += String(UserVar[event->BaseVarIndex], 0);
 
         if (state == false)
         {
           unsigned long rotationTime = timePassedSince(Plugin_242_triggerTimePrevious);
           Plugin_242_triggerTime = rotationTime;
-          log = F(" Zeit (ms): ");
-          log += String(rotationTime, 0);
+          log += F(" Zeit (ms): ");
+          log += rotationTime;
 
           unsigned long msPerHour = 3600 * 1000;
 
@@ -163,14 +193,20 @@ boolean Plugin_242(byte function, struct EventStruct *event, String &string)
           Plugin_242_triggerTimePrevious = millis();
 
           // 3600 / gestoppte Durchschnittszeit / 75 U/kWh = aktueller laufender Verbrauch
-          log = F(" Verbrauch (WH): ");
+          log += F(" Verbrauch (WH): ");
           log += UserVar[event->BaseVarIndex + 1];
+        } 
+        else 
+        {
+          // reset sum to eleminate value drift
+          Plugin_242_cumulativeSum = 0;
         }
 
         sendData(event);
       }
 
       // reset Values
+      Plugin_242_avgValuePrevious = Plugin_242_avgValue;
       Plugin_242_MeasurementCount = 0;
       Plugin_242_valueSum = 0;
 
@@ -178,35 +214,34 @@ boolean Plugin_242(byte function, struct EventStruct *event, String &string)
         sendData(event);
       }
 
-      log += F(" Raw: ");
-      log += UserVar[event->BaseVarIndex + 2];
+      addLog(LOG_LEVEL_INFO, log);
 
+      success = true;
+      break;
     }
 
-    addLog(LOG_LEVEL_INFO, log);
-    addLog(LOG_LEVEL_DEBUG, debuglog);
+    case PLUGIN_READ:
+    {
+      // We do not actually read the value as this is already done 10x/second
+      // Instead we just send the last known state stored in Uservar
+      String log = F("PLUGIN_READ Triggerstate: ");
+      log += String(UserVar[event->BaseVarIndex]);
+      log += F(" Consuption: ");
+      log += String(UserVar[event->BaseVarIndex + 1]);
+      log += F(" Raw: ");
+      log += String(UserVar[event->BaseVarIndex + 2]);
+      addLog(LOG_LEVEL_INFO, log);
 
-    success = true;
-    break;
+      if( Plugin_242_triggerstate ){
+        // reset sum to eleminate value drift
+        Plugin_242_cumulativeSum = 0;
+      }
+
+      success = true;
+      break;
+    }
   }
-
-case PLUGIN_READ:
-{
-  // We do not actually read the value as this is already done 10x/second
-  // Instead we just send the last known state stored in Uservar
-  String log = F("PLUGIN_READ Triggerstate: ");
-  log += String(UserVar[event->BaseVarIndex]);
-  log = F(" Consuption: ");
-  log += String(UserVar[event->BaseVarIndex + 1]);
-  log += F(" Raw: ");
-  log += String(UserVar[event->BaseVarIndex + 2]);
-  addLog(LOG_LEVEL_INFO, log);
-
-  success = true;
-  break;
-}
-}
-return success;
+  return success;
 }
 
 //#endif
